517a8cbae56f776fa30088f6d8a7cfe09b33fb82
Sarvesh Maurya <sarveshmaurya7307@gmail.com>
2026-06-05 23:32:08 +0530

    Solved Week 1 Day 5

---
+++ week_1/Day_5/ValidPalindrome.java
@@ -0,0 +1,31 @@
+package week_1.Day_5;
+
+public class ValidPalindrome {
+
+    public static boolean isPalindrome(String s) {
+
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        String s = "A man, a plan, a canal: Panama";
+
+        System.out.println(isPalindrome(s));
+    }
+}
